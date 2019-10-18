36a339b5b4b69b22c4aff001d9d464ae82e6434f
BabyDragon003 <golddragon01234@gmail.com>
2019-10-18 10:19:32 +0900

    "2019-10-18 10:19"

--- src/index.tsx
+++ src/index.tsx
@@ -8,5 +8,13 @@ import { store } from './Redux/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
